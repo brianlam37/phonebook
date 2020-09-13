62e75837c4fa27592ca02e0873164077d93b9f39
brianlam37 <brianlam37@gmail.com>
2020-09-13 04:49:44 -0700

    modified for part3

--- src/App.js
+++ src/App.js
@@ -21,80 +21,58 @@ const App = () => {
 
     const handleClick = (e) =>{
         e.preventDefault();
-        let exists = persons.find((person)=>person.name === newName);
-
-        if(exists!== undefined){
-            if(window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)){
-                let index = persons.findIndex(p => p.id === exists.id);
-                const newPerson = {
-                    name: newName, 
-                    number: newNumber
-                }
-                personService
-                    .update(exists.id,newPerson).then(personResponse=>{
-                        let copy = [...persons];
-                        copy[index] = newPerson;
-                        setPersons(copy);
-                        setNewName('');
-                        setNewNumber('');
-                    })
-                    .catch(error => {
-                        setMessage(
-                            `Information of ${exists.name} has already been removed from server`
-                        );
-                        setMessageType('error');
-                        setTimeout(() => {
-                            setMessage(null);
-                            setMessageType(null);
-                        }, 5000)
-                        setPersons(persons.filter(p => p.id !== exists.id))
-                    });
-            }
-        }else if(persons.find((person)=>person.number === newNumber)!== undefined){
+        const newPerson = {
+            name: newName, 
+            number: newNumber
+        }
+        personService
+        .create(newPerson)
+        .then(personResponse =>{
             setMessage(
-                `${newNumber} is already added to phonebook`
+                `Added ${newName}`
+            )
+            setMessageType('success');
+            setTimeout(() => {
+                setMessage(null)
+                setMessageType(null);
+            }, 5000)
+            setPersons(persons.concat(personResponse));
+            setNewName('');
+            setNewNumber('');
+        }).catch(error => {
+            setMessage(
+                `There was an error adding ${newName}`
             )
             setMessageType('error');
             setTimeout(() => {
                 setMessage(null)
-                setMessageType(null)
+                setMessageType(null);
             }, 5000)
-        }else{
-            const newPerson = {
-                name: newName, 
-                number: newNumber
-            }
-            personService
-            .create(newPerson)
-            .then(personResponse =>{
-                setMessage(
-                    `Added ${newName}`
-                )
+        });
+    }
+
+    const handleDeleteUpdate = (person) =>{
+        let copy = persons.filter(p => p.id !== person.id);
+        if(window.confirm(`Delete ${person.name}?`)){
+            personService.remove(person.id).then(personResponse=>{
+                setPersons(copy);
+                setMessage(`Deleted ${person.name}`);
                 setMessageType('success');
                 setTimeout(() => {
                     setMessage(null)
                     setMessageType(null);
                 }, 5000)
-                setPersons(persons.concat(personResponse));
-                setNewName('');
-                setNewNumber('');
-            }).catch(error => {
-                setMessage(
-                    `There was an error adding ${newName}`
-                )
+            }).catch(error =>{
+                setPersons(copy);
+                setMessage(`Information of ${person.name} has already been removed from server`);
                 setMessageType('error');
                 setTimeout(() => {
                     setMessage(null)
                     setMessageType(null);
                 }, 5000)
-                setPersons(persons.filter(p => p.id !== exists.id))
             });
         }
-    }
 
-    const handleDeleteUpdate = (filter) =>{
-        let copy = persons.filter(person => person.id !== filter.id);
-        setPersons(copy);
     }
     const handleFilterChange = (event) =>{
         setFilter(event.target.value)
@@ -122,7 +100,7 @@ const App = () => {
                 handleNumberChange = {handleNumberChange}
             />
             <h3>Numbers</h3>
-            <Persons handleDeleteUpdate = {handleDeleteUpdate} persons = {persons.filter(p => p.name.toLowerCase().includes(filter.toLowerCase()))} setMessage = {setMessage} setMessageType = {setMessageType}></Persons>
+            <Persons handleClick = {handleDeleteUpdate} persons = {persons.filter(p => p.name.toLowerCase().includes(filter.toLowerCase()))}/>
 
         </div>
     )
--- src/components/Persons.js
+++ src/components/Persons.js
@@ -1,35 +1,14 @@
 import React from 'react';
-import personService from '../services/persons'
-const Number = ({person,handleDeleteUpdate, setMessage, setMessageType}) =>{
-    const handleClick = ()=>{
-        if(window.confirm(`Delete ${person.name}?`)){
-            personService.remove(person.id).then(personResponse=>{
-                handleDeleteUpdate(person);
-                setMessage(`Deleted ${person.name}`);
-                setMessageType('success');
-                setTimeout(() => {
-                    setMessage(null)
-                    setMessageType(null);
-                }, 5000)
-            }).catch(error =>{
-                handleDeleteUpdate(person);
-                setMessage(`Information of ${person.name} has already been removed from server`);
-                setMessageType('error');
-                setTimeout(() => {
-                    setMessage(null)
-                    setMessageType(null);
-                }, 5000)
-            });
-        }
-    }
+
+const Number = ({person,handleClick}) =>{
     return(
-        <p>{person.name} {person.number} <button onClick={handleClick}>delete</button></p>
+        <p>{person.name} {person.number} <button onClick={() => handleClick(person)}>delete</button></p>
     )
 }
-const Persons = ({persons, handleDeleteUpdate, setMessage, setMessageType}) =>{
+const Persons = ({persons, handleClick}) =>{
     return(
         <>
-            {persons.map(p => (<Number key={p.name} handleDeleteUpdate = {handleDeleteUpdate} person = {p} setMessage = {setMessage} setMessageType = {setMessageType}/>))}
+            {persons.map(p => (<Number key={p.name} handleClick = {handleClick} person = {p}/>))}
         </>
     )
 }
